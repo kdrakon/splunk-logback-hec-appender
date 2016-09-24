413d400a58b505bfdffd1d05c26f66ead412f2ed
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2016-09-24 20:12:18 +1000

    increasing timeouts for tests

--- src/test/scala/io/policarp/logback/SplunkHecAppenderBaseTest.scala
+++ src/test/scala/io/policarp/logback/SplunkHecAppenderBaseTest.scala
@@ -6,7 +6,7 @@ import monix.eval.Task
 import monix.execution.Scheduler
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{ Seconds, Span }
+import org.scalatest.time.{ Minute, Seconds, Span }
 import org.scalatest.{ Matchers, WordSpec }
 import org.slf4j.LoggerFactory
 
@@ -80,7 +80,7 @@ trait SplunkHecAppenderBaseTest extends WordSpec with Matchers with ScalaFutures
         }
       }
 
-      whenReady(f, Timeout(Span(30, Seconds))) { r =>
+      whenReady(f, Timeout(Span(1, Minute))) { r =>
         appender.fakeRequest.executions.get() shouldBe executions
       }
     }
@@ -120,7 +120,7 @@ class SplunkHecAppenderTestWithSpillingStrategy extends WordSpec with Matchers w
         }
       }
 
-      whenReady(f, Timeout(Span(30, Seconds))) { r =>
+      whenReady(f, Timeout(Span(1, Minute))) { r =>
         logger.info(s"Logged ${appender.fakeRequest.executions.get()} messages")
         assert(appender.fakeRequest.executions.get() >= minExecutions)
       }
